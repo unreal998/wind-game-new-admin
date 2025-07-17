5e908837523b8ee4dc06da3cbd98a8341eafac6e
NikitaGavr <nikita2gavrilenko@gmail.com>
2025-07-17 02:53:16 +0200

    added text input option

--- src/components/EnhancedDatePicker.tsx
+++ src/components/EnhancedDatePicker.tsx
@@ -1,15 +1,24 @@
 import { TRANSLATIONS_DATEPICKER } from "@/components/data-table/constants"
 import { DateRangePicker } from "@/components/DatePicker"
 import { uk } from "date-fns/locale"
-import React, { Dispatch, SetStateAction } from "react"
+import React, { Dispatch, SetStateAction, useState } from "react"
 import { type DateRange } from "react-day-picker"
 import { subDays } from "date-fns"
+import { Button } from "./Button"
 
 export function EnhancedDatePicker({
   setSelectedDateRange,
 }: {
   setSelectedDateRange?: Dispatch<SetStateAction<DateRange | undefined>>
 }) {
+  const [open, setOpen] = useState<boolean>(false)
+  const [tempDateInput, setTempDateInput] = useState<{
+    from: string
+    to: string
+  }>({
+    from: "",
+    to: "",
+  })
   const [selectedDates, setSelectedDates] = React.useState<
     DateRange | undefined
   >({
@@ -20,13 +29,43 @@ export function EnhancedDatePicker({
   const maxDate = new Date()
   const minDate = subDays(new Date(), 365)
 
+  const handleDatePickerInput = () => {
+    console.log(!tempDateInput.from, !tempDateInput.to)
+    if (!tempDateInput.from || !tempDateInput.to) {
+      alert("Помилка: Не всі дата інпути заповненi")
+      return
+    }
+    if (
+      new Date(tempDateInput.from).getFullYear() > 2025 ||
+      new Date(tempDateInput.to).getFullYear() > 2025
+    ) {
+      alert("Помилка: Не вибирайте рок більше теперішнього")
+      return
+    }
+
+    if (new Date(tempDateInput.from) > new Date(tempDateInput.to)) {
+      alert("Помилка: Дата ВІД є вища ніж ДО")
+      return
+    }
+
+    setSelectedDates({
+      from: new Date(tempDateInput.from),
+      to: new Date(tempDateInput.to),
+    })
+  }
+
   return (
-    <div className="flex w-full justify-between">
+    <div className="flex w-full items-center justify-between">
       <div className="w-full sm:flex sm:items-center sm:gap-2">
         <DateRangePicker
           setSelectedDateRange={setSelectedDateRange}
           value={selectedDates}
-          onChange={(dates) => setSelectedDates(dates)}
+          onChange={(dates) => {
+            setSelectedDates(dates)
+            if (setSelectedDateRange && dates) {
+              setSelectedDateRange(dates)
+            }
+          }}
           className="w-full sm:w-fit"
           toDate={maxDate}
           fromDate={minDate}
@@ -35,6 +74,44 @@ export function EnhancedDatePicker({
           translations={TRANSLATIONS_DATEPICKER}
         />
       </div>
+      {!open && (
+        <Button onClick={() => setOpen(true)}>Обрати дату текстом</Button>
+      )}
+      {open && (
+        <div className="m-2 flex flex-col items-center gap-2">
+          <div className="flex items-center gap-1 self-end">
+            <span>Від:</span>
+            <input
+              type="date"
+              className="rounded border border-gray-600 bg-black px-2 py-1 text-white"
+              onChange={(e) =>
+                setTempDateInput((prev) => ({ ...prev, from: e.target.value }))
+              }
+              value={tempDateInput.from}
+            />
+          </div>
+          <div className="flex items-center gap-1 self-end">
+            <span>До:</span>
+            <input
+              type="date"
+              className="rounded border border-gray-600 bg-black px-2 py-1 text-white"
+              onChange={(e) =>
+                setTempDateInput((prev) => ({ ...prev, to: e.target.value }))
+              }
+              value={tempDateInput.to}
+            />
+          </div>
+          <Button
+            onClick={() => {
+              handleDatePickerInput()
+              setOpen(false)
+            }}
+            className="rounded px-4 py-2"
+          >
+            Обрати
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
