422e68f3f72cb6e879aa1c334d5104dac2d4f70e
Maksym Solonyi <unreal9981@gmail.com>
2025-07-19 16:38:32 +0300

    wip

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -4,11 +4,6 @@ import { SignUpDto } from "@/stores/useAuthStore"
 import { createClient } from "@supabase/supabase-js"
 
 export async function createUser(userData: SignUpDto) {
-  console.log("NEXT_PUBLIC_SUPABASE_URL", process.env.NEXT_PUBLIC_SUPABASE_URL!)
-  console.log(
-    "SUPABASE_SERVICE_ROLE_KEY",
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  )
 
   const supabaseAdmin = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
