732477fc81fcb64d50584a3c90edaa4dd3c0febe
Maksym Solonyi <unreal9981@gmail.com>
2025-11-21 01:16:25 +0200

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ const ALLOWED_IPS = [
   "5.45.79.91",
   "46.227.136.53",
   "185.213.234.87",
-  "81.30.165.224"//my ip
+  "94.153.46.215"//my ip
 ];
 
 export async function middleware(request: NextRequest) {
