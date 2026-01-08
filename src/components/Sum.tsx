b4a1c8ee0b0211a1d9f95da753d56b2aad971665
Maksym Solonyi <unreal9981@gmail.com>
2026-01-08 21:04:47 +0200

    numbers fix

--- src/components/Sum.tsx
+++ src/components/Sum.tsx
@@ -1,7 +1,8 @@
 export default function Sum({ sum, label }: { sum: number; label: string }) {
+  console.log(sum)
   return (
     <h1 className="text-1xl m-1 bg-gray-200 p-2 font-semibold text-gray-900 dark:bg-gray-900 dark:text-gray-50">
-      {label}: {(Math.round(sum * 100) / 100).toFixed(2)}
+      {label}: {(Math.floor(sum * 100) / 100).toFixed(2)}
     </h1>
   )
 }
